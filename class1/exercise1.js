d0e8b7d67fefa59fc47fd660631ff39948ccd5be
Róger Ferreira <139197687+rogergsf@users.noreply.github.com>
2024-07-30 11:54:29 -0300

    Add files via upload

---
+++ class1/exercise1.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+const personName = `Roger Gustavo Silveira Ferreira`;
+const personAge = 18;
+const isStudent = true;
+
+console.log(`I am ${personName}, I'm ${personAge} years old and am I a student? ${isStudent}`);
+=======
+const personName = `Roger Gustavo Silveira Ferreira`;
+const personAge = 18;
+const isStudent = true;
+
+console.log(`I am ${personName}, I'm ${personAge} years old and am I a student? ${isStudent}`);
+>>>>>>> 1ff6d13774dc46e7266b58706154a15f95555f13
---
+++ class1/exercise2.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+// // String to Number
+// const num1 = '50';
+// const num2 = '70';
+// const newNum1 = Number(num1);
+// const newNum2 = Number(num2);
+// const soma = newNum1 + newNum2;
+// console.log(soma);
+
+// // Number to String
+// const num3 = 5;
+// const num3toString = num3.toString();
+// console.log(typeof num3toString);
+
+const readlineSync = require('readline-sync')
+
+const name = readlineSync.question("What's your name? ");
+console.log(`Hello ${name}`);
+=======
+// // String to Number
+// const num1 = '50';
+// const num2 = '70';
+// const newNum1 = Number(num1);
+// const newNum2 = Number(num2);
+// const soma = newNum1 + newNum2;
+// console.log(soma);
+
+// // Number to String
+// const num3 = 5;
+// const num3toString = num3.toString();
+// console.log(typeof num3toString);
+
+const readlineSync = require('readline-sync')
+
+const name = readlineSync.question("What's your name? ");
+console.log(`Hello ${name}`);
+>>>>>>> 1ff6d13774dc46e7266b58706154a15f95555f13
