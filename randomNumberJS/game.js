6d62652c81ba9e55c0293f40e36a626221991714
Róger <rogerferreira610@gmail.com>
2024-08-10 12:20:43 -0300

    Adding folder randomNumber

---
+++ randomNumberJS/game.js
@@ -0,0 +1,18 @@
+const rl = require('readline-sync');
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let guess;
+
+do {
+    guess = rl.questionInt('Choose a number between 1 and 100: ');
+
+    if (guess < 1 || guess > 100) {
+        console.log('Invalid number! Please enter a number between 1 and 100.\n');
+    } else if (guess < randomNumber) {
+        console.log('Wrong! The number is higher.\n');
+    } else if (guess > randomNumber) {
+        console.log('Wrong! The number is smaller.\n');
+    }
+} while (guess !== randomNumber);
+
+console.log('You got it right!\nCongrats :)');
